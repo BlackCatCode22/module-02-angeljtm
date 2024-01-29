3ec8599c73c006f4fa33a61a92bef2473d90ce41
Angel <103345941+angeljtm@users.noreply.github.com>
2024-01-29 14:09:04 -0800

    Add files via upload

--- MadnessWithMethods.java
+++ MadnessWithMethods.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 public class MadnessWithMethods {
     public static int getAnIntFromTheUser(){
         Scanner myObj = new Scanner(System.in);
-        System.out.print("Enter an integer: ");
+        System.out.print("Enter an integer: "); // asking for int
         int userInt = myObj.nextInt();
-        return userInt;
+        return userInt; //return int
     }
 
     public static int compareTwoInts(int a, int b){
-        if(a < b)
+        if(a < b)  //if b is greater, it returns b
             return b;
-        else return a;
+        else return a; //if b isn't larger, return a
     }
 
     public static int sumTwoInts(int a, int b){
-       return a+b;
+       return a+b; //adds the two ints together
     }
 
     public static void main(String[] args) {
